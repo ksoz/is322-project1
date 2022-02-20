aeee16b010e15b15c3d94d9e18cf490f6875d7ed
Kyle Sosnicki <kylesosnicki@kyle-soz-popos.myfiosgateway.com>
2022-02-20 18:19:10 -0500

    current work

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+// Import dependencies
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+//basic component
+const App = () => {
+    return <div> 
+        <h1>This is a new div.</h1>
+        <h2>hello world</h2>
+        </div>
+}
+
+
+//render component onto DOM
+/*
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#container')
+)
+*/
